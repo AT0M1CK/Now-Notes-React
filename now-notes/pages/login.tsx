7056ff933ab22d6a753a287a97d1ecc019fc1936
Akshayanand Vm <50578479+AT0M1CK@users.noreply.github.com>
2023-05-14 17:24:36 +0530

    refactor: add right side cover to login page

--- now-notes/pages/login.tsx
+++ now-notes/pages/login.tsx
@@ -1,9 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
 import LoginForm from "@/components/Form/LoginForm";
 import RegisterForm from "@/components/Form/RegisterForm";
 import TextInput from "@/components/TextInput";
 import React, { useState } from "react";
 import { auth } from "@/firebase/firebaseConfig";
 import ResetForm from "@/components/Form/ResetForm";
+import Cover from "../assets/CoverImage.jpg";
+import Cover2 from "../assets/CoverImage2.jpg";
+import Cover3 from "../assets/CoverImage3.jpg";
+import Cover4 from "../assets/CoverImage4.jpg";
+import Cover5 from "../assets/CoverImage5.jpg";
+import Image from "next/image";
 
 export enum LoginState {
   LOGIN,
@@ -24,8 +31,8 @@ const LoginPage = () => {
     <>
       <div className="min-h-screen flex">
         <div className="flex-1 ...">
-          <div className="flex flex-row min-h-screen bg-gray-600 justify-center items-center">
-            <div className="flex flex-col h-128 w-128 p-10 md:w-128 sm:w-96 m-10  justify-between rounded-lg shadow-lg items-center bg-gray-200">
+          <div className="flex flex-row min-h-screen shadow-inner bg-slate-500 justify-center items-center">
+            <div className="flex flex-col h-128 w-128 p-10 md:w-128 sm:w-96 m-10  justify-between rounded-lg shadow-xl items-center bg-gray-200">
               {/* span div */}
 
               {/* input div  */}
@@ -48,7 +55,16 @@ const LoginPage = () => {
           </div>
         </div>
         <div className="flex-1 ...">
-          <div className="flex flex-row min-h-screen bg-slate-200 justify-center items-center"></div>
+          <div className="flex min-h-screen bg-slate-200 justify-center shadow-2xl items-center">
+            {/* <img src="/assets/CoverImage.jpg" alt="Cover" /> */}
+            <Image
+              className=" h-screen w-screen"
+              src={Cover5}
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
         </div>
       </div>
     </>
